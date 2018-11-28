945db61bf6acb6d41cff306b70e4bece85741ec7
zachmann <gabriel.zachmann@kit.edu>
2018-11-28 15:15:47 +0100

    using saved and new crypt paramteres correctly

--- src/utils/crypt.c
+++ src/utils/crypt.c
@@ -15,6 +15,7 @@ struct cryptParameter {
   size_t nonce_len;
   size_t salt_len;
   size_t mac_len;
+  size_t key_len;
   int    base64_variant;
   int    hash_ops_limit;
   int    hash_mem_limit;
@@ -23,10 +24,9 @@ struct cryptParameter {
 
 struct cryptParameter newCryptParameters() {
   return (struct cryptParameter){
-      NONCE_LEN,         SALT_LEN,
-      MAC_LEN,           sodium_base64_VARIANT_ORIGINAL,
-      PW_HASH_OPS_LIMIT, PW_HASH_MEMLIMIT,
-      PW_HASH_ALG};
+      SODIUM_NONCE_LEN,        SODIUM_SALT_LEN,       SODIUM_MAC_LEN,
+      SODIUM_KEY_LEN,          SODIUM_BASE64_VARIANT, SODIUM_PW_HASH_OPSLIMIT,
+      SODIUM_PW_HASH_MEMLIMIT, SODIUM_PW_HASH_ALG};
 }
 
 struct encryptionInfo {
@@ -58,12 +58,16 @@ void secFreeEncryptionInfo(struct encryptionInfo crypt) {
 struct encryptionInfo _crypt_encrypt(const unsigned char* text,
                                      const char*          password) {
   syslog(LOG_AUTHPRIV | LOG_DEBUG, "Encrypt using base64 encoding");
-  char nonce[NONCE_LEN];
-  randombytes_buf(nonce, NONCE_LEN);
-  char* salt_base64 = secAlloc(
-      sodium_base64_ENCODED_LEN(SALT_LEN, sodium_base64_VARIANT_ORIGINAL) + 1);
-  unsigned char  ciphertext[MAC_LEN + strlen((char*)text)];
-  struct key_set keys = crypt_keyDerivation_base64(password, salt_base64, 1);
+  char nonce[SODIUM_NONCE_LEN];
+  randombytes_buf(nonce, SODIUM_NONCE_LEN);
+  char* salt_base64 =
+      secAlloc(sodium_base64_ENCODED_LEN(SODIUM_SALT_LEN,
+                                         sodium_base64_VARIANT_ORIGINAL) +
+               1);
+  unsigned char         ciphertext[SODIUM_MAC_LEN + strlen((char*)text)];
+  struct cryptParameter cryptParams = newCryptParameters();
+  struct key_set        keys =
+      crypt_keyDerivation_base64(password, salt_base64, 1, &cryptParams);
   if (keys.encryption_key == NULL) {
     secFree(salt_base64);
     secFree(keys.hash_key);
@@ -79,12 +83,12 @@ struct encryptionInfo _crypt_encrypt(const unsigned char* text,
   }
   secFree(keys.encryption_key);
   char* ciphertext_base64 =
-      toBase64((char*)ciphertext, MAC_LEN + strlen((char*)text));
-  char* hash_key_base64 = toBase64(keys.hash_key, KEY_LEN);
+      toBase64((char*)ciphertext, SODIUM_MAC_LEN + strlen((char*)text));
+  char* hash_key_base64 = toBase64(keys.hash_key, SODIUM_KEY_LEN);
   secFree(keys.hash_key);
-  char* nonce_base64 = toBase64(nonce, NONCE_LEN);
+  char* nonce_base64 = toBase64(nonce, SODIUM_NONCE_LEN);
   return (struct encryptionInfo){ciphertext_base64, nonce_base64, salt_base64,
-                                 hash_key_base64, newCryptParameters()};
+                                 hash_key_base64, cryptParams};
 }
 
 char* crypt_encrypt(const char* text, const char* password) {
@@ -100,14 +104,15 @@ char* crypt_encrypt(const char* text, const char* password) {
   // 5 cipher_base64
   // 6 hash_key_base64
   // [7 version] // Not included here
-  const char* const fmt        = "%lu\n%s\n%s\n%lu:%lu:%lu:%d:%d:%d:%d\n%s\n%s";
+  const char* const fmt = "%lu\n%s\n%s\n%lu:%lu:%lu:%lu:%d:%d:%d:%d\n%s\n%s";
   size_t            cipher_len = strlen(text) + cry.cryptParameter.mac_len;
   char*             ret        = oidc_sprintf(
       fmt, cipher_len, cry.nonce_base64, cry.salt_base64,
       cry.cryptParameter.nonce_len, cry.cryptParameter.salt_len,
-      cry.cryptParameter.mac_len, cry.cryptParameter.base64_variant,
-      cry.cryptParameter.hash_ops_limit, cry.cryptParameter.hash_mem_limit,
-      cry.cryptParameter.hash_alg, cry.encrypted_base64, cry.hash_key_base64);
+      cry.cryptParameter.mac_len, cry.cryptParameter.key_len,
+      cry.cryptParameter.base64_variant, cry.cryptParameter.hash_ops_limit,
+      cry.cryptParameter.hash_mem_limit, cry.cryptParameter.hash_alg,
+      cry.encrypted_base64, cry.hash_key_base64);
   secFreeEncryptionInfo(cry);
   return ret;
 }
@@ -138,9 +143,10 @@ unsigned char* crypt_decrypt_base64(struct encryptionInfo crypt,
     oidc_errno = OIDC_ECRYPM;
     return NULL;
   }
-  struct key_set keys =
-      crypt_keyDerivation_base64(password, crypt.salt_base64, 0);
-  char* computed_hash_key_base64 = toBase64(keys.hash_key, KEY_LEN);
+  struct key_set keys = crypt_keyDerivation_base64(password, crypt.salt_base64,
+                                                   0, &(crypt.cryptParameter));
+  char*          computed_hash_key_base64 =
+      toBase64(keys.hash_key, crypt.cryptParameter.key_len);
   secFree(keys.hash_key);
   if (sodium_memcmp(computed_hash_key_base64, crypt.hash_key_base64,
                     strlen(crypt.hash_key_base64)) != 0) {
@@ -151,18 +157,18 @@ unsigned char* crypt_decrypt_base64(struct encryptionInfo crypt,
   }
   secFree(computed_hash_key_base64);
 
-  unsigned char nonce[NONCE_LEN];
+  unsigned char nonce[crypt.cryptParameter.nonce_len];
   unsigned char ciphertext[cipher_len];
-  sodium_base642bin(
-      nonce, NONCE_LEN, crypt.nonce_base64,
-      sodium_base64_ENCODED_LEN(NONCE_LEN, sodium_base64_VARIANT_ORIGINAL),
-      NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL);
+  sodium_base642bin(nonce, crypt.cryptParameter.nonce_len, crypt.nonce_base64,
+                    sodium_base64_ENCODED_LEN(crypt.cryptParameter.nonce_len,
+                                              sodium_base64_VARIANT_ORIGINAL),
+                    NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL);
   sodium_base642bin(
       ciphertext, cipher_len, crypt.encrypted_base64,
       sodium_base64_ENCODED_LEN(cipher_len, sodium_base64_VARIANT_ORIGINAL),
       NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL);
-  unsigned char* decrypted =
-      secAlloc(sizeof(unsigned char) * (cipher_len - MAC_LEN + 1));
+  unsigned char* decrypted = secAlloc(
+      sizeof(unsigned char) * (cipher_len - crypt.cryptParameter.mac_len + 1));
   if (crypto_secretbox_open_easy(decrypted, ciphertext, cipher_len, nonce,
                                  (unsigned char*)keys.encryption_key) != 0) {
     secFree(keys.encryption_key);
@@ -191,10 +197,10 @@ char* crypt_decryptFromList(list_t* lines, const char* password) {
   crypt.encrypted_base64 = oidc_strcopy(list_at(lines, 4)->val);
   crypt.hash_key_base64  = oidc_strcopy(list_at(lines, 5)->val);
   char*             tmp  = list_at(lines, 3)->val;
-  const char* const fmt  = "%lu:%lu:%lu:%d:%d:%d:%d";
+  const char* const fmt  = "%lu:%lu:%lu:%lu:%d:%d:%d:%d";
   sscanf(tmp, fmt, &crypt.cryptParameter.nonce_len,
          &crypt.cryptParameter.salt_len, &crypt.cryptParameter.mac_len,
-         &crypt.cryptParameter.base64_variant,
+         &crypt.cryptParameter.key_len, &crypt.cryptParameter.base64_variant,
          &crypt.cryptParameter.hash_ops_limit,
          &crypt.cryptParameter.hash_mem_limit, &crypt.cryptParameter.hash_alg);
   char* ret = (char*)crypt_decrypt_base64(crypt, cipher_len, password);
@@ -237,19 +243,20 @@ char* crypt_decrypt(const char* crypt_str, const char* password) {
  */
 unsigned char* crypt_decrypt_hex(char* ciphertext_hex, unsigned long cipher_len,
                                  const char* password,
-                                 char        nonce_hex[2 * NONCE_LEN + 1],
-                                 char        salt_hex[2 * SALT_LEN + 1]) {
-  if (cipher_len < MAC_LEN) {
+                                 char        nonce_hex[2 * LEG_NONCE_LEN + 1],
+                                 char        salt_hex[2 * LEG_SALT_LEN + 1]) {
+  if (cipher_len < LEG_MAC_LEN) {
     oidc_errno = OIDC_ECRYPM;
     return NULL;
   }
   syslog(LOG_AUTHPRIV | LOG_DEBUG, "Decrypt using hex encoding");
   unsigned char* decrypted =
-      secAlloc(sizeof(unsigned char) * (cipher_len - MAC_LEN + 1));
+      secAlloc(sizeof(unsigned char) * (cipher_len - LEG_MAC_LEN + 1));
   unsigned char* key = crypt_keyDerivation_hex(password, salt_hex, 0);
-  unsigned char  nonce[NONCE_LEN];
+  unsigned char  nonce[LEG_NONCE_LEN];
   unsigned char  ciphertext[cipher_len];
-  sodium_hex2bin(nonce, NONCE_LEN, nonce_hex, 2 * NONCE_LEN, NULL, NULL, NULL);
+  sodium_hex2bin(nonce, LEG_NONCE_LEN, nonce_hex, 2 * LEG_NONCE_LEN, NULL, NULL,
+                 NULL);
   sodium_hex2bin(ciphertext, cipher_len, ciphertext_hex, 2 * cipher_len, NULL,
                  NULL, NULL);
   if (crypto_secretbox_open_easy(decrypted, ciphertext, cipher_len, nonce,
@@ -269,31 +276,39 @@ unsigned char* crypt_decrypt_hex(char* ciphertext_hex, unsigned long cipher_len,
 /**
  * @brief derivates a key from the given password
  * @param password the password use for key derivation
- * @param salt_hex a pointer to a 2*SALT_LEN+1 big buffer. If @p generateNewSalt
- * is set, the generated salt will be stored here, otherwise the stored salt
- * will be used
+ * @param salt_hex a pointer to a 2*LEG_SALT_LEN+1 big buffer. If @p
+ * generateNewSalt is set, the generated salt will be stored here, otherwise the
+ * stored salt will be used
  * @param generateNewSalt indicates if a new salt should be generated or if
  * @p salt_hex should be used. If you use this function for encryption
  * @p generateNewSalt should be @c 1; for decryption @c 0
  * @return a pointer to the derivated key. It has to be freed after usage.
- * @note this function is only used to keyDerivation with hex encoded salt
+ * @note this function is only used for keyDerivation with hex encoded salt
  * (before version 2.1.0) - see also @c crypt_keyDerivation_base64
+ * @deprecated use this function only to derivate a key to compare it to one
+ * derivate before version 2.1.0; it is deprecated to use it for new keys.
  */
 unsigned char* crypt_keyDerivation_hex(const char* password,
-                                       char        salt_hex[2 * SALT_LEN + 1],
-                                       int         generateNewSalt) {
+                                       char salt_hex[2 * LEG_SALT_LEN + 1],
+                                       int  generateNewSalt) {
   syslog(LOG_AUTHPRIV | LOG_DEBUG, "Derivate key using hex encoding");
-  unsigned char* key = secAlloc(sizeof(unsigned char) * (KEY_LEN + 1));
-  unsigned char  salt[SALT_LEN];
+  if (generateNewSalt == 1) {
+    syslog(LOG_AUTHPRIV | LOG_WARNING, "%s is deprecated", __func__);
+    printImportant("%s is deprecated", __func__);
+  }
+  unsigned char* key = secAlloc(sizeof(unsigned char) * (LEG_KEY_LEN + 1));
+  unsigned char  salt[LEG_SALT_LEN];
   if (generateNewSalt) {
     /* Choose a random salt */
-    randombytes_buf(salt, SALT_LEN);
-    sodium_bin2hex(salt_hex, 2 * SALT_LEN + 1, salt, SALT_LEN);
+    randombytes_buf(salt, LEG_SALT_LEN);
+    sodium_bin2hex(salt_hex, 2 * LEG_SALT_LEN + 1, salt, LEG_SALT_LEN);
   } else {
-    sodium_hex2bin(salt, SALT_LEN, salt_hex, 2 * SALT_LEN, NULL, NULL, NULL);
+    sodium_hex2bin(salt, LEG_SALT_LEN, salt_hex, 2 * LEG_SALT_LEN, NULL, NULL,
+                   NULL);
   }
-  if (crypto_pwhash(key, KEY_LEN, password, strlen(password), salt,
-                    PW_HASH_OPS_LIMIT, PW_HASH_MEMLIMIT, PW_HASH_ALG) != 0) {
+  if (crypto_pwhash(key, LEG_KEY_LEN, password, strlen(password), salt,
+                    LEG_PW_HASH_OPSLIMIT, LEG_PW_HASH_MEMLIMIT,
+                    LEG_PW_HASH_ALG) != 0) {
     secFree(key);
     syslog(LOG_AUTHPRIV | LOG_ALERT,
            "Could not derivate key. Probably because system out of memory.\n");
@@ -325,29 +340,29 @@ char* toBase64(const char* bin, size_t len) {
  * @note this function is only used to keyDerivation with base64 encoded salt
  * (since version 2.1.0) - see also @c crypt_keyDerivation_hex
  */
-struct key_set crypt_keyDerivation_base64(
-    const char* password,
-    char        salt_base64[sodium_base64_ENCODED_LEN(SALT_LEN,
-                                               sodium_base64_VARIANT_ORIGINAL) +
-                     1],
-    int         generateNewSalt) {
+struct key_set crypt_keyDerivation_base64(const char* password,
+                                          char        salt_base64[],
+                                          int         generateNewSalt,
+                                          struct cryptParameter* cryptParams) {
   syslog(LOG_AUTHPRIV | LOG_DEBUG, "Derivate key using base64 encoding");
-  char*         key = secAlloc(sizeof(unsigned char) * (2 * KEY_LEN + 1));
-  unsigned char salt[SALT_LEN];
+  char* key = secAlloc(sizeof(unsigned char) * (2 * cryptParams->key_len + 1));
+  unsigned char salt[cryptParams->key_len];
   if (generateNewSalt) {
     /* Choose a random salt */
-    randombytes_buf(salt, SALT_LEN);
+    randombytes_buf(salt, cryptParams->salt_len);
     sodium_bin2base64(
         salt_base64,
-        sodium_base64_ENCODED_LEN(SALT_LEN, sodium_base64_VARIANT_ORIGINAL) + 1,
-        salt, SALT_LEN, sodium_base64_VARIANT_ORIGINAL);
+        sodium_base64_ENCODED_LEN(cryptParams->salt_len,
+                                  sodium_base64_VARIANT_ORIGINAL) +
+            1,
+        salt, cryptParams->salt_len, sodium_base64_VARIANT_ORIGINAL);
   } else {
-    sodium_base642bin(
-        salt, SALT_LEN, salt_base64,
-        sodium_base64_ENCODED_LEN(SALT_LEN, sodium_base64_VARIANT_ORIGINAL),
-        NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL);
+    sodium_base642bin(salt, cryptParams->salt_len, salt_base64,
+                      sodium_base64_ENCODED_LEN(cryptParams->salt_len,
+                                                sodium_base64_VARIANT_ORIGINAL),
+                      NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL);
   }
-  if (crypto_pwhash((unsigned char*)key, 2 * KEY_LEN, password,
+  if (crypto_pwhash((unsigned char*)key, 2 * cryptParams->key_len, password,
                     strlen(password), salt, crypto_pwhash_OPSLIMIT_INTERACTIVE,
                     crypto_pwhash_MEMLIMIT_INTERACTIVE,
                     crypto_pwhash_ALG_DEFAULT) != 0) {
@@ -357,8 +372,9 @@ struct key_set crypt_keyDerivation_base64(
     oidc_errno = OIDC_EMEM;
     return (struct key_set){NULL, NULL};
   }
-  char* encryption_key = oidc_memcopy(key, KEY_LEN);
-  char* hash_key       = oidc_memcopy(key + KEY_LEN, KEY_LEN);
+  char* encryption_key = oidc_memcopy(key, cryptParams->key_len);
+  char* hash_key =
+      oidc_memcopy(key + cryptParams->key_len, cryptParams->key_len);
   secFree(key);
   struct key_set keys = {encryption_key, hash_key};
   return keys;
