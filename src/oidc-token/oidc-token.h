11caf258ea90c570f583e3db05b3f366490d6324
zachmann <gabriel.zachmann@kit.edu>
2019-01-07 07:46:28 +0100

    pass argument struct as pointer in oidc-token

--- src/oidc-token/oidc-token.h
+++ src/oidc-token/oidc-token.h
@@ -10,6 +10,6 @@ const char* argp_program_version = TOKEN_VERSION;
 
 const char* argp_program_bug_address = BUG_ADDRESS;
 
-void printEnvCommands(struct arguments      arguments,
+void printEnvCommands(struct arguments*     arguments,
                       struct token_response response);
 #endif  // OIDC_TOKEN_H
