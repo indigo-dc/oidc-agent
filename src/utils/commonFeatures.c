be1eda3a113ba14f7de74d54112044118ddc8087
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2020-01-30 12:11:46 -0600

    skip header in ssh-add -l

--- src/utils/commonFeatures.c
+++ src/utils/commonFeatures.c
@@ -13,7 +13,7 @@ void common_handleListConfiguredAccountConfigs() {
   list_mergeSort(list, (int (*)(const void*, const void*))compareFilesByName);
   char* str = listToDelimitedString(list, '\n');
   list_destroy(list);
-  printStdout("The following account configurations are usable: \n%s\n", str);
+  printStdout("%s\n", str);
   secFree(str);
 }
 
