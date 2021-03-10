6879bfa4c0f0b625b608d3399a809a7d5571f97c
zachmann <gabriel.zachmann@kit.edu>
2021-03-10 09:58:10 +0100

    remove unused tests

--- test/src/utils/json/suite.c
+++ test/src/utils/json/suite.c
@@ -1,12 +1,10 @@
 #include "suite.h"
 #include "tc_isJSONObject.h"
 #include "tc_setJSONValue.h"
-// TODO
 
 Suite* test_suite_json() {
   Suite* ts_json = suite_create("json");
   suite_add_tcase(ts_json, test_case_isJSONObject());
   suite_add_tcase(ts_json, test_case_setJSONValue());
-  // TODO
   return ts_json;
 }
