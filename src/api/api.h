1a2a7c1b7273c8a51dbe60cfd9d397846549a324
zachmann <gabriel.zachmann@kit.edu>
2023-05-17 16:27:27 +0200

    [clib] drop old functions + renaming

--- src/api/api.h
+++ src/api/api.h
@@ -1,13 +1,11 @@
 #ifndef OIDC_AGENT_API_H
 #define OIDC_AGENT_API_H
 
+#include "accounts.h"
 #include "error.h"
 #include "memory.h"
 #include "mytokens.h"
 #include "response.h"
 #include "tokens.h"
-#if defined __MINGW32__ || defined __MINGW64__ || defined _WIN32
-#include "accounts.h"
-#endif
 
 #endif  // OIDC_AGENT_API_H
