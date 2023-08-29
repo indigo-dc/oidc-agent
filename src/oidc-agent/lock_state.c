7ac71c62c7c0a36d133b453501ac354a528c0a8d
zachmann <gabriel.zachmann@kit.edu>
2023-08-29 17:13:46 +0200

    increase delay penalty for wrong agent unlocks

--- src/oidc-agent/lock_state.c
+++ src/oidc-agent/lock_state.c
@@ -22,11 +22,10 @@ oidc_error_t unlock(const char* password) {
   if (!strequal(agent_state.lock_state.hash, hash)) {
     secFree(hash);
     oidc_errno = OIDC_EPASS;
-    /* delay in 0.1s increments up to 10s */
-    if (fail_count < 100) {
+    if (fail_count < 25) {
       fail_count++;
     }
-    unsigned int delay = 100 * fail_count;
+    unsigned int delay = 100 * fail_count * fail_count;
     agent_log(DEBUG, "unlock failed, delaying %0.1lf seconds",
               (double)delay / 1000);
     msleep(delay);
