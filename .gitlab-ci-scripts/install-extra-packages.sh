e5f5602904029e11ea4af9336de6925b3e5c595f
Marcus <marcus@hardt-it.de>
2024-08-22 11:56:37 +0200

    add gstreamer1 packages to fix dependencies

--- .gitlab-ci-scripts/install-extra-packages.sh
+++ .gitlab-ci-scripts/install-extra-packages.sh
@@ -21,12 +21,15 @@ case ${DISTRO} in
             "8.7")
                 yum clean all
                 dnf upgrade almalinux-release --nogpgcheck
-                yum -y install clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel
+                yum -y install clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel\
+                    gstreamer1 gstreamer1-plugins-base
+
             ;;
             9)
-                yum clean all
-                dnf upgrade almalinux-release --nogpgcheck
-                yum -y install clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel
+                yum -y clean all
+                dnf -y upgrade almalinux-release --nogpgcheck
+                yum -y install clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel\
+                    gstreamer1 gstreamer1-plugins-base
             ;;
         esac
         ;;
@@ -34,7 +37,9 @@ case ${DISTRO} in
         case ${RELEASE} in
             9)
                 yum clean all
-                yum -y install cjson-devel clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel
+                yum -y install cjson-devel clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel\
+                    gstreamer1 gstreamer1-plugins-base
+
             ;;
         esac
         ;;
