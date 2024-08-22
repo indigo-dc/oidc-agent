800ccddde61ee6bf3e2f1e5bec1680aa01400b25
Marcus <marcus@hardt-it.de>
2024-08-22 12:20:56 +0200

    add -y to yum calls

--- .gitlab-ci-scripts/install-extra-packages.sh
+++ .gitlab-ci-scripts/install-extra-packages.sh
@@ -3,7 +3,7 @@ case ${DISTRO} in
     fedora)
         case ${RELEASE} in
             38|39)
-                yum clean all
+                yum -y clean all
                 yum -y install clibs-list-devel
                 ;;
         esac
@@ -11,7 +11,7 @@ case ${DISTRO} in
     centos)
         case ${RELEASE} in
             7)
-                yum clean all
+                yum -y clean all
                 yum -y install clibs-list-devel
             ;;
         esac
@@ -19,8 +19,8 @@ case ${DISTRO} in
     almalinux)
         case ${RELEASE} in
             "8.7")
-                yum clean all
-                dnf upgrade almalinux-release --nogpgcheck
+                yum -y clean all
+                dnf -y upgrade almalinux-release --nogpgcheck
                 yum -y install clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel\
                     gstreamer1 gstreamer1-plugins-base
 
@@ -36,7 +36,7 @@ case ${DISTRO} in
     rockylinux)
         case ${RELEASE} in
             9)
-                yum clean all
+                yum -y clean all
                 yum -y install cjson-devel clibs-list-devel curl-devel systemd-rpm-macros webkit2gtk3-devel\
                     gstreamer1 gstreamer1-plugins-base
 
