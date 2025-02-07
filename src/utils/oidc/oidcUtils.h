381b3674a3db9fde4f267f7bf6203ae66fa98625
zachmann <gabriel.zachmann@kit.edu>
2025-02-07 16:11:32 +0100

    do not request `offline_access` in a token refresh request; fix #613

--- src/utils/oidc/oidcUtils.h
+++ src/utils/oidc/oidcUtils.h
@@ -1,6 +1,6 @@
 #ifndef OIDC_AGENT_OIDCUTILS_H
 #define OIDC_AGENT_OIDCUTILS_H
 
-char* removeScope(char* scopes, char* rem);
+char* removeScope(char* scopes, const char* rem);
 
 #endif  // OIDC_AGENT_OIDCUTILS_H
