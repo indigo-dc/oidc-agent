f92493d97aeb6c9c37ce778dca35cceb03747fb5
zachmann <gabriel.zachmann@kit.edu>
2026-09-01 12:11:30 +0200

    fix ghsa-r42c-xpvp-8fj3

--- test/src/utils/uriUtils/suite.c
+++ test/src/utils/uriUtils/suite.c
@@ -2,10 +2,12 @@
 
 #include "tc_codeStateFromURI.h"
 #include "tc_extractParameterValueFromUri.h"
+#include "tc_isValidAuthEndpointUrl.h"
 
 Suite* test_suite_uriUtils() {
   Suite* ts_uriUtils = suite_create("uriUtils");
   suite_add_tcase(ts_uriUtils, test_case_codeStateFromURI());
   suite_add_tcase(ts_uriUtils, test_case_extractParameterValueFromUri());
+  suite_add_tcase(ts_uriUtils, test_case_isValidAuthEndpointUrl());
   return ts_uriUtils;
 }
